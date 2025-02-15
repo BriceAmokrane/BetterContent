eb630be2f37c11876251c813b37f5678703bb1f7
BriceAmokrane <briceamokranedev@gmail.com>
2025-02-16 00:55:18 +0100

    feat(core) : get .env variables

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,16 @@ import (
 	"BetterContent/internal/handlers"
 	"BetterContent/internal/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load .env file")
+	}
 
 	router := gin.Default()
 
@@ -16,7 +23,7 @@ func main() {
 	router.POST("/addContentLink", func(context *gin.Context) {
 		contentHandler.HandleAddContentLink(context)
 	})
-	err := router.Run()
+	err = router.Run()
 
 	if err != nil {
 		return
